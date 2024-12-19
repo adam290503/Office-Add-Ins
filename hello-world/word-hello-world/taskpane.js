8afaae802b61f1acf7b751b410140849a72da36c
amitgt1976 <amitg@acubed.it>
2024-12-19 08:25:27 +0000

    fixed trhe error

--- hello-world/word-hello-world/taskpane.js
+++ hello-world/word-hello-world/taskpane.js
@@ -90,9 +90,8 @@ async function decryptHighlightedOOXML() {
   (async function () {
     try {
       const EncryptedData = await getSpecificXmlNode("Key001");
-      settimeout(() => { console.log("Retrieved Value:", EncryptedData);},1000);
+      setTimeout(() => { console.log("Retrieved Value:", EncryptedData);},1000);
 
-      
       // The value is now accessible as a string
       await Word.run(async (context) => {
     
